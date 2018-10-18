c1a215bb8597c62efe5661b0c5dd16eaf9363ea9
Dizy Zeng <mhisf@vip.qq.com>
2018-10-18 09:59:55 +0800

    wip

--- TextSegmentation.py
+++ TextSegmentation.py
@@ -1,6 +1,7 @@
 import csv
 import jieba
 import jieba.analyse
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def stopwordslist():
@@ -14,18 +15,22 @@ def segmentation():
         csv_file = csv.reader(f, dialect='excel')
         stopwords = stopwordslist()
         pre_row = ''
-        text_segmentation = ''
+        text_segmentation = []
         for (line, row) in enumerate(csv_file):
+            text_segmentation.append([])
             # 除重
             if row[1] != pre_row:
                 # 分词
                 seg_list = jieba.cut(row[1], cut_all=False)
                 for word in seg_list:
                     if word not in stopwords:
-                        text_segmentation += ' ' + word
-                text_segmentation += '\n'
+                        text_segmentation[line].append(word)
+            if text_segmentation[line].__len__() == 0:
+                pass
+                # print(row[1] + ' empty segment')
+                # print(seg_list)
             pre_row = row[1]
-        # print(text_segmentation)
+        print(text_segmentation)
 
 
 if __name__ == '__main__':
