7fedb4b348233bf2c09d655170c0876fd1df23e3
Dizy Zeng <mhisf@vip.qq.com>
2018-10-18 13:26:17 +0800

    add vectorize

--- TextSegmentation.py
+++ TextSegmentation.py
@@ -3,35 +3,52 @@ import jieba
 import jieba.analyse
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+STOP_WORD_LIST_PATH = 'stop_words.txt'
+TRAIN_DATA_PATH = 'train.csv'
+
+
+def flatten(list):
+    return [item for sublist in list for item in sublist]
+
 
 def stopwordslist():
-    stopwords = [line.strip() for line in open('stop_words.txt').readlines()]
+    stopwords = [line.strip()
+                 for line in open(STOP_WORD_LIST_PATH).readlines()]
     return stopwords
 
 
-def segmentation():
-    with open('train.csv', mode='r', encoding='utf-8', newline='') as f:
+def segment():
+    with open(TRAIN_DATA_PATH, mode='r', encoding='utf-8', newline='') as f:
         # 读取csv文件
         csv_file = csv.reader(f, dialect='excel')
         stopwords = stopwordslist()
         pre_row = ''
         text_segmentation = []
         for (line, row) in enumerate(csv_file):
-            text_segmentation.append([])
             # 除重
             if row[1] != pre_row:
+                line_segmentation = []
                 # 分词
                 seg_list = jieba.cut(row[1], cut_all=False)
                 for word in seg_list:
                     if word not in stopwords:
-                        text_segmentation[line].append(word)
-            if text_segmentation[line].__len__() == 0:
-                pass
-                # print(row[1] + ' empty segment')
-                # print(seg_list)
+                        line_segmentation.append(word)
+                text_segmentation.append(line_segmentation)
             pre_row = row[1]
-        print(text_segmentation)
+        return text_segmentation
 
 
+def tfidf_vectorize(corpus):
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit_transform(corpus)
+    print(tfidf)
+
+
+def main():
+    segments = segment()
+    tfidf_vectorize(flatten(segments))
+
+
+ad
 if __name__ == '__main__':
-    segmentation()
+    main()
